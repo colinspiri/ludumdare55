8c018a69e4e16098f7e3805223ce5dbe36a667f8
Colin Spiridonov <colinspiri@gmail.com>
2024-04-12 22:45:49 -0700

    main menu speedrun

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // constants
+    [SerializeField] UIConstants uiConstants;
+    public List<MenuScreen> menuScreens;
+    public UnityEvent defaultBackAction;
+
+    private PlayerInputActions _inputActions;
+    
+    // state
+    private List<MenuScreen> _menuScreenStack = new List<MenuScreen>();
+
+    private void OnEnable()
+    {
+        _inputActions = new PlayerInputActions();
+        _inputActions.Enable();
+
+        if (menuScreens.Count > 0)
+        {
+            // hide all menu screens
+            foreach (var screen in menuScreens)
+            {
+                screen.gameObject.SetActive(true);
+                screen.StartOffscreen();
+            }
+
+            // show starting menu screen
+            var startingScreen = menuScreens[0];
+            startingScreen.StartOnscreen();
+
+            _menuScreenStack.Clear();
+            _menuScreenStack.Add(startingScreen);
+        }
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void OnDisable()
+    {
+        _menuScreenStack.Clear();
+    }
+
+    private void Update()
+    {
+        if (_inputActions.UI.Cancel.triggered)
+        {
+            BackToPreviousMenuScreen();
+
+            // if (AudioManager.Instance) AudioManager.Instance.PlayBackSound();
+        }
+
+        if (_inputActions != null && _inputActions.Player.Move.triggered) {
+            if(_menuScreenStack.Count > 0) _menuScreenStack[^1].CheckSelectable();
+        }
+    }
+
+    public void OpenMenuScreen(MenuScreen screen)
+    {
+        // disable current menu screen
+        _menuScreenStack[^1].PutAway();
+
+        // push new menu screen to stack & enable
+        _menuScreenStack.Add(screen);
+        screen.Push();
+    }
+
+    public void BackToPreviousMenuScreen()
+    {
+        // if at root menu screen, do default back action instead
+        if (_menuScreenStack.Count <= 1)
+        {
+            defaultBackAction?.Invoke();
+            return;
+        }
+        
+        // Set the last button so it comes back for it
+        // _menuScreenStack[^1].SetLastButton(_menuScreenStack[^1].GetCurrentSelectable());
+
+        // disable current menu screen & pop from stack
+        _menuScreenStack[^1].Pop();
+        _menuScreenStack.RemoveAt(_menuScreenStack.Count - 1);
+
+        // enable previous menu screen
+        _menuScreenStack[^1].BringBack();
+    }
+
+    public void Quit() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void ActivateMenu(GameObject menu)
+    {
+        menu.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void DeactivateMenu(GameObject menu)
+    {
+        menu.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "SceneLoader", menuName = "SceneLoader")]
+public class SceneLoader : ScriptableObject {
+    public SceneReference mainMenuScene;
+    public SceneReference gameScene;
+
+    public void DebugTest(string testString) {
+        Debug.Log("test " + testString + " at " + Time.time);
+    }
+
+    public void LoadGameScene() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(gameScene.ScenePath);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(mainMenuScene.ScenePath);
+    }
+
+    public void Quit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
