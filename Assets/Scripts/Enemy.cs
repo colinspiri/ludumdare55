5d13ccb82f27ff98dffbe63841fa157784bda513
Brooklyn Humphrey <69836100+BrooklynBHumphrey@users.noreply.github.com>
2024-04-15 01:04:26 -0700

    Added enemy pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
     public float speed;
     public ParticleSystem deathParticles;
+    private AIPath path;
+
+    private void Start()
+    {
+        path = GetComponent<AIPath>();
+    }
 
     void Update()
     {
         var player = PlayerController.Instance;
         if (player != null)
         {
-            Vector2 direction = player.transform.position - transform.position;
+/*            Vector2 direction = player.transform.position - transform.position;
             direction.Normalize();
 
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position,
                 speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);*/
+
+            path.maxSpeed = speed;
+            path.destination = player.transform.position;
         }
     }
 
