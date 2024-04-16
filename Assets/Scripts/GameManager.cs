3694891ad589709a631ac755ec79b5352a237613
Colin Spiridonov <colinspiri@gmail.com>
2024-04-15 17:25:47 -0700

    made more of the level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using ScriptableObjectArchitecture;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,12 +8,19 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private GameObjectCollection allEnemies;
 
     private bool _gameWon;
+    [SerializeField] private float onWinDelay;
     public UnityEvent onWin;
     
     private void Update() {
         if (allEnemies.Count == 0 && _gameWon == false) {
             _gameWon = true;
-            onWin?.Invoke();
+            StartCoroutine(OnWinCoroutine());
         }
+    }
+    
+    private IEnumerator OnWinCoroutine() {
+        yield return new WaitForSeconds(onWinDelay);
+
+        onWin?.Invoke();
     }
 }
